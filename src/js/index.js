1298eccd4ac254f8cac28caed28bab6f0385ac27
Almuhandis <abubakaralmuhandis@gmail.com>
2021-09-12 03:07:01 -0700

    forkify new update

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,36 @@
-import Search from './models/Search';
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 /** global state of the app
  * - search object
  * - current recipe object
- * - shopping list object 
- * - liked recipes 
+ * - shopping list object
+ * - liked recipes
  */
-const state = {}
+const state = {};
 
-const controlSearch =  async () => {
-    // 1. Get query from view 
-    const query = 'pizza' // TODO
+const controlSearch = async () => {
+  // 1. Get query from view
+  const query = searchView.getInput();
+  if (query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
 
-    if(query){
-        // 2. New search object and add to state 
-        state.search = new Search(query);
+    // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-        // 3. Prepare UI for results
+    // 4. Search for recipes
+    await state.search.getResults();
 
-        // 4. Search for recipes 
-       await state.search.getResults();
+    // 5. Render Results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
 
-        // 5. Render Results on UI 
-        console.log(state.search.result);
-    }
-}
-
-document.querySelector('.search').addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
-
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
 });
